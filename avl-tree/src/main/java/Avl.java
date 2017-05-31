dc7fce98c5d47294c765df6c3ea60a768377bd3f
Korskov98 <ka1998@mail.ru>
2017-05-31 17:14:29 +0300

    исправлено

--- avl-tree/src/main/java/Avl.java
+++ avl-tree/src/main/java/Avl.java
@@ -1,27 +1,227 @@
-public class Avl {
+import java.util.*;
 
-    private class Node{
-        int key;
-        Node left;
-        Node right;
+public class Avl<T extends Comparable<T>> implements SortedSet<T>{
+
+    private class Node<T>{
+        T key;
+        Node<T> left;
+        Node<T> right;
         int height;
 
-        Node(int key) {this.key = key; left = right = null; height = 1; }
+        Node(T key) {
+            this.key = key;
+            left = right = null;
+            height = 1;
+        }
     }
 
-    private Node root;
-    private int size;
+    private Node<T> root;
+    private int size = 0;
 
-    Avl(){
+    public Avl(){
         root = null;
+    }
+
+    public SortedSet<T> subSet(T fromElement, T toElement) {
+        Iterator<T> iterator = iterator();
+        Avl<T> new_tree = new Avl<T>();
+        int from_number = getIndexNumber(fromElement);
+        int to_number = getIndexNumber(toElement);
+        for (int i = 0; i <= from_number; ++i){
+            iterator.next();
+        }
+        for (int i = from_number + 1; i < to_number; ++i){
+            new_tree.add(iterator.next());
+        }
+        return new_tree;
+    }
+
+    public SortedSet<T> headSet(T toElement) {
+        Iterator<T> iterator = iterator();
+        Avl<T> new_tree = new Avl<T>();
+        int number = getIndexNumber(toElement);
+        for (int i = 0; i < number; ++i){
+            new_tree.add(iterator.next());
+        }
+        return new_tree;
+    }
+
+    public SortedSet<T> tailSet(T fromElement) {
+        Iterator<T> iterator = iterator();
+        Avl<T> new_tree = new Avl<T>();
+        int number = getIndexNumber(fromElement);
+        for (int i = 0; i <= number; ++i){
+            iterator.next();
+        }
+        for (int i = number + 1; i < size; ++i){
+            new_tree.add(iterator.next());
+        }
+        return new_tree;
+    }
+
+    private int getIndexNumber(T key){
+        int number = 0;
+        Iterator<T> iterator = iterator();
+        while ((iterator.hasNext()) && (iterator.next() != key)){
+            ++number;
+        }
+        return number;
+    }
+
+    public T first(){
+        Node<T> node = root;
+        while (node.left != null) {
+            node = node.left;
+        }
+        return node.key;
+    }
+
+    public T last() {
+        Node<T> node = root;
+        while (node.right != null){
+            node = node.right;
+        }
+        return node.key;
+    }
+
+    @Override
+    public Comparator<? super T> comparator() {
+        return null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    public boolean contains(Object o) {
+        T key = (T) o;
+        Node<T> node = root;
+        while (node != null){
+            if (node.key == key) {
+                return true;
+            }else if (key.compareTo(node.key) < 0) {
+                node = node.left;
+            }else if (key.compareTo(node.key) > 0) {
+                node = node.right;
+            }
+        }
+        return false;
+    }
+
+    public Iterator<T> iterator() {
+        return new AvlTreeIterator();
+    }
+
+    public Object[] toArray() {
+        Object[] r = new Object[size];
+        Iterator<T> iterator = iterator();
+        for (int i = 0; i < r.length; ++i){
+            if (!iterator.hasNext()){
+                return Arrays.copyOf(r, i);
+            }
+            r[i] = iterator.next();
+        }
+        return r;
+    }
+
+    public <T1> T1[] toArray(T1[] a) {
+        int size = size();
+        T1[] r = a.length >= size ? a : (T1[]) java.lang.reflect.Array.newInstance(a.getClass().getComponentType(), size);
+        Iterator<T> iterator = iterator();
+        for (int i = 0; i < r.length; i++) {
+            if (! iterator.hasNext()) {
+                if (a != r){
+                    return Arrays.copyOf(r, i);
+                }
+                r[i] = null;
+                return r;
+            }
+            r[i] = (T1)iterator.next();
+        }
+        return r;
+    }
+
+    public boolean containsAll(Collection<?> c) {
+        Iterator<?> iterator = c.iterator();
+        while (iterator.hasNext()){
+            if (!contains(iterator.next())){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean addAll(Collection<? extends T> c) {
+        boolean flag = false;
+        Iterator<? extends T> iterator = c.iterator();
+        while (iterator.hasNext()){
+            if (add(iterator.next())){
+                flag = true;
+            }
+        }
+        return flag;
+    }
+
+    public boolean retainAll(Collection<?> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean removeAll(Collection<?> c) {
+        boolean flag = false;
+        Iterator<?> iterator = c.iterator();
+        while (iterator.hasNext()){
+            if (remove(iterator.next())){
+                flag = true;
+            }
+        }
+        return flag;
+    }
+
+    public void clear() {
+        Iterator<T> iterator = iterator();
+        while (iterator.hasNext()){
+            remove(iterator.next());
+        }
+    }
+
+    public boolean equals(Object o) {
+        if (o == this){
+            return true;
+        }
+        if (!(o instanceof Set)){
+            return false;
+        }
+        Collection c = (Collection) o;
+        if (c.size() != size){
+            return false;
+        }
+        try {
+            return containsAll(c);
+        }catch (ClassCastException e){
+            return false;
+        }catch (NullPointerException e){
+            return false;
+        }
+    }
+
+    public int hashCode() {
+        int h = 0;
+        Iterator<T> iterator = iterator();
+        while (iterator.hasNext()){
+            T node = iterator.next();
+            if (node != null){
+                h += node.hashCode();
+            }
+        }
+        return h;
     }
 
     int getHeight(){
         return root.height;
-    }
-
-    int getSize(){
-        return size;
     }
 
     private int getHeight(Node p){
@@ -82,25 +282,66 @@ public class Avl {
         return p;
     }
 
-    void add(int k){
+    public boolean add(T k){
         if (root == null){
             root = new Node(k);
-        }else {
-            if (k < root.key) {
+            ++size;
+            return true;
+        }else if (k.compareTo(root.key) < 0){
                 root.left = add(root.left, k);
-            } else {
+                root = balanceTree(root);
+                ++size;
+                return true;
+            } else if(k.compareTo(root.key) > 0){
                 root.right = add(root.right, k);
+                root = balanceTree(root);
+                ++size;
+                return true;
             }
-        }
-        root = balanceTree(root);
-        ++size;
+        return false;
     }
 
-    private Node add(Node p, int k){
+    public boolean remove(Object o) {
+        T key = null;
+        try {
+            key = (T) o;
+        } catch (ClassCastException e) {
+            return false;
+        }
+        if (!(contains(key))){
+            return false;
+        }else{
+            root = delete(root,key);
+            --size;
+            return true;
+        }
+    }
+
+    private Node delete(Node<T> p, Object o){
+        T key = (T) o;
+        if (key.compareTo(p.key) < 0){
+            p.left = delete(p.left, o);
+        }else if (key.compareTo(p.key) > 0){
+            p.right = delete(p.right, o);
+        }else {
+            Node left = p.left;
+            Node right = p.right;
+            if (right == null){
+                return left;
+            }
+            Node min = findMin(right);
+            min.right = deleteMin(right);
+            min.left = left;
+            return balanceTree(min);
+        }
+        return balanceTree(p);
+    }
+
+    private Node add(Node p, T k){
         if (p == null){
             return new Node(k);
         }
-        if (k < p.key){
+        if (k.compareTo(root.key) < 0){
             p.left = add(p.left, k);
         }else{
             p.right = add(p.right, k);
@@ -124,46 +365,37 @@ public class Avl {
         return balanceTree(p);
     }
 
-    void delete(int k){
-        if (k < root.key){
-            root.left = delete(root.left, k);
-        }else{
-            if (k > root.key){
-                root.right = delete(root.right, k);
-            }else {
-                Node left = root.left;
-                Node right = root.right;
-                if (right == null){
-                    root = left;
-                }
-                Node min = findMin(right);
-                min.right = deleteMin(right);
-                min.left = left;
-                root = balanceTree(min);
-            }
-        }
-        root = balanceTree(root);
-        --size;
-    }
+    private class AvlTreeIterator implements Iterator<T>{
 
-    private Node delete(Node p, int k){
-        if (k < p.key){
-            p.left = delete(p.left, k);
-        }else{
-            if (k > p.key){
-                p.right = delete(p.right, k);
-            }else {
-                Node left = p.left;
-                Node right = p.right;
-                if (right == null){
-                    return left;
-                }
-                Node min = findMin(right);
-                min.right = deleteMin(right);
-                min.left = left;
-                return balanceTree(min);
+        private final Stack<Node<T>> nodes;
+
+        public AvlTreeIterator(){
+            this.nodes = new Stack<Node<T>>();
+            pushLeft(root);
+        }
+
+        private void pushLeft(Node<T> node) {
+            while (node != null) {
+                nodes.push(node);
+                node = node.left;
             }
         }
-        return balanceTree(p);
+
+        public boolean hasNext() {
+            return !nodes.isEmpty();
+        }
+
+        public T next() {
+            Node<T> node = nodes.pop();
+            if (node != null){
+                pushLeft(node.right);
+                return node.key;
+            }
+            throw new NoSuchElementException();
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
     }
 }
--- avl-tree/src/test/java/AvlTest.java
+++ avl-tree/src/test/java/AvlTest.java
@@ -1,21 +1,25 @@
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
 import static org.junit.Assert.*;
 
 public class AvlTest {
     @Test
     public void addTest1(){
-        Avl tree = new Avl();
+        Avl<Integer> tree = new Avl<Integer>();
         tree.add(5);
         tree.add(4);
         tree.add(6);
         tree.add(1);
         assertEquals(3,tree.getHeight());
-        assertEquals(4,tree.getSize());
+        assertEquals(4, tree.size());
     }
 
     @Test
     public void addTest2(){
-        Avl tree = new Avl();
+        Avl<Integer> tree = new Avl<Integer>();
         tree.add(9);
         tree.add(6);
         tree.add(5);
@@ -23,33 +27,361 @@ public class AvlTest {
         tree.add(2);
         tree.add(1);
         assertEquals(3,tree.getHeight());
-        assertEquals(6,tree.getSize());
+        assertEquals(6,tree.size());
+    }
+
+    @Test
+    public void addAllTest1(){
+        Avl<Integer> tree = new Avl<Integer>();
+        ArrayList<Integer> array = new ArrayList<Integer>();
+        array.add(5);
+        array.add(4);
+        array.add(6);
+        array.add(1);
+        tree.addAll(array);
+        assertEquals(3,tree.getHeight());
+        assertEquals(4, tree.size());
+    }
+
+    @Test
+    public void addAllTest2(){
+        Avl<Integer> tree = new Avl<Integer>();
+        ArrayList<Integer> array = new ArrayList<Integer>();
+        array.add(9);
+        array.add(6);
+        array.add(5);
+        array.add(3);
+        array.add(2);
+        array.add(1);
+        tree.addAll(array);
+        assertEquals(3,tree.getHeight());
+        assertEquals(6,tree.size());
     }
 
     @Test
     public void deleteTest1(){
-        Avl tree = new Avl();
+        Avl<Integer> tree = new Avl<Integer>();
         tree.add(5);
         tree.add(4);
         tree.add(6);
         tree.add(1);
-        tree.delete(1);
+        tree.remove(1);
         assertEquals(2,tree.getHeight());
-        assertEquals(3,tree.getSize());
+        assertEquals(3,tree.size());
     }
 
     @Test
     public void deleteTest2(){
-        Avl tree = new Avl();
+        Avl<Integer> tree = new Avl<Integer>();
         tree.add(9);
         tree.add(6);
         tree.add(5);
         tree.add(3);
         tree.add(2);
         tree.add(1);
-        tree.delete(1);
-        tree.delete(6);
+        tree.remove(1);
+        tree.remove(6);
         assertEquals(3,tree.getHeight());
-        assertEquals(4,tree.getSize());
+        assertEquals(4,tree.size());
+    }
+
+    @Test
+    public void deleteTest3(){
+        Avl<Integer> tree = new Avl<Integer>();
+        tree.add(9);
+        tree.add(6);
+        tree.add(5);
+        tree.add(3);
+        tree.add(2);
+        tree.add(1);
+        assertFalse(tree.remove(0));
+        assertFalse(tree.remove(10));
+        assertTrue(tree.remove(1));
+    }
+
+    @Test
+    public void deleteAllTest1(){
+        Avl<Integer> tree = new Avl<Integer>();
+        ArrayList<Integer> array = new ArrayList<Integer>();
+        array.add(5);
+        array.add(4);
+        tree.add(5);
+        tree.add(4);
+        tree.add(6);
+        tree.add(1);
+        tree.removeAll(array);
+        assertTrue(tree.contains(1));
+    }
+
+    @Test
+    public void deleteAllTest2(){
+        Avl<Integer> tree = new Avl<Integer>();
+        ArrayList<Integer> array = new ArrayList<Integer>();
+        array.add(9);
+        array.add(8);
+        tree.add(5);
+        tree.add(4);
+        tree.add(6);
+        tree.add(1);
+        tree.removeAll(array);
+        assertEquals(4,tree.size());
+    }
+
+    @Test
+    public void isEmptyTest1(){
+        Avl<Integer> tree = new Avl<Integer>();
+        assertTrue(tree.isEmpty());
+        tree.add(9);
+        tree.add(80);
+        assertFalse(tree.isEmpty());
+        tree.remove(9);
+        assertFalse(tree.isEmpty());
+    }
+
+    @Test
+    public void isEmptyTest2(){
+        Avl<Integer> tree = new Avl<Integer>();
+        assertTrue(tree.isEmpty());
+        tree.add(9);
+        tree.add(80);
+        assertFalse(tree.isEmpty());
+        tree.clear();
+        assertTrue(tree.isEmpty());
+    }
+
+    @Test
+    public void firstTest1(){
+        Avl<Integer> tree = new Avl<Integer>();
+        tree.add(9);
+        tree.add(80);
+        assertEquals(9,(long)tree.first());
+        tree.remove(9);
+        assertEquals(80,(long)tree.first());
+    }
+
+    @Test
+    public void firstTest2(){
+        Avl<Integer> tree = new Avl<Integer>();
+        tree.add(1);
+        tree.add(80);
+        assertEquals(1,(long)tree.first());
+        tree.remove(80);
+        assertEquals(1,(long)tree.first());
+    }
+
+    @Test
+    public void lastTest1(){
+        Avl<Integer> tree = new Avl<Integer>();
+        tree.add(9);
+        tree.add(80);
+        assertEquals(80,(long)tree.last());
+        tree.remove(9);
+        assertEquals(80,(long)tree.last());
+    }
+
+    @Test
+    public void lastTest2(){
+        Avl<Integer> tree = new Avl<Integer>();
+        tree.add(9);
+        tree.add(90);
+        assertEquals(90,(long)tree.last());
+        tree.remove(90);
+        assertEquals(9,(long)tree.last());
+    }
+
+    @Test
+    public void containTest1(){
+        Avl<Integer> tree = new Avl<Integer>();
+        tree.add(9);
+        tree.add(80);
+        assertTrue(tree.contains(80));
+        tree.remove(9);
+        assertFalse(tree.contains(9));
+    }
+
+    @Test
+    public void containTest2(){
+        Avl<Integer> tree = new Avl<Integer>();
+        tree.add(90);
+        tree.add(80);
+        assertTrue(tree.contains(90));
+        assertTrue(tree.contains(80));
+    }
+
+    @Test
+    public void containsAllTest1(){
+        Avl<Integer> tree = new Avl<Integer>();
+        ArrayList<Integer> array = new ArrayList<Integer>();
+        array.add(9);
+        array.add(80);
+        tree.add(9);
+        tree.add(80);
+        assertTrue(tree.containsAll(array));
+        tree.remove(9);
+        assertFalse(tree.containsAll(array));
+    }
+
+    @Test
+    public void containsAllTest2(){
+        Avl<Integer> tree = new Avl<Integer>();
+        ArrayList<Integer> array = new ArrayList<Integer>();
+        array.add(9);
+        array.add(80);
+        tree.add(9);
+        tree.add(900);
+        assertFalse(tree.containsAll(array));
+        tree.remove(9);
+        assertFalse(tree.containsAll(array));
+    }
+
+    @Test
+    public void clearTest1(){
+        Avl<Integer> tree = new Avl<Integer>();
+        tree.add(9);
+        tree.add(80);
+        tree.clear();
+        assertFalse(tree.contains(80));
+    }
+
+    @Test
+    public void clearTest2(){
+        Avl<Integer> tree = new Avl<Integer>();
+        tree.add(9);
+        tree.add(80);
+        tree.clear();
+        assertTrue(tree.isEmpty());
+    }
+
+    @Test
+    public void hashCodeTest1(){
+        Avl<Integer> tree = new Avl<Integer>();
+        tree.add(9);
+        tree.add(80);
+        Avl<Integer> copy = tree;
+        assertEquals(tree.hashCode(), copy.hashCode());
+    }
+
+    @Test
+    public void headSetTest1(){
+        Avl<Integer> tree = new Avl<Integer>();
+        tree.add(9);
+        tree.add(80);
+        tree.add(0);
+        Avl<Integer> head = (Avl<Integer>) tree.headSet(80);
+        assertTrue(head.contains(0));
+        assertFalse(head.contains(80));
+    }
+
+    @Test
+    public void headSetTest2(){
+        Avl<Integer> tree = new Avl<Integer>();
+        tree.add(900);
+        tree.add(80);
+        tree.add(0);
+        Avl<Integer> head = (Avl<Integer>) tree.headSet(0);
+        assertTrue(head.isEmpty());
+    }
+
+    @Test
+    public void tailSetTest1(){
+        Avl<Integer> tree = new Avl<Integer>();
+        tree.add(9);
+        tree.add(80);
+        tree.add(0);
+        Avl<Integer> tail = (Avl<Integer>) tree.tailSet(9);
+        assertTrue(tail.contains(80));
+        assertFalse(tail.contains(0));
+    }
+
+    @Test
+    public void tailSetTest2(){
+        Avl<Integer> tree = new Avl<Integer>();
+        tree.add(9);
+        tree.add(80);
+        tree.add(0);
+        Avl<Integer> tail = (Avl<Integer>) tree.tailSet(80);
+        assertTrue(tail.isEmpty());
+    }
+
+    @Test
+    public void subSetTest1(){
+        Avl<Integer> tree = new Avl<Integer>();
+        tree.add(80);
+        tree.add(8);
+        tree.add(6);
+        tree.add(5);
+        tree.add(90);
+        tree.add(0);
+        Avl<Integer> sub = (Avl<Integer>) tree.subSet(5, 80);
+        assertFalse(sub.contains(80));
+        assertFalse(sub.contains(0));
+        assertTrue(sub.contains(8));
+    }
+
+    @Test
+    public void subSetTest2(){
+        Avl<Integer> tree = new Avl<Integer>();
+        tree.add(80);
+        tree.add(8);
+        tree.add(6);
+        tree.add(5);
+        tree.add(90);
+        tree.add(0);
+        Avl<Integer> sub = (Avl<Integer>) tree.subSet(80, 90);
+        assertTrue(sub.isEmpty());
+    }
+
+    @Test
+    public void testIterator(){
+        Avl<Integer> tree = new Avl<Integer>();
+        assertTrue(tree.isEmpty());
+        tree.add(5);
+        tree.add(9);
+        tree.add(3);
+        int[] array = new int[tree.size()];
+        int[ ] array2 = {3, 5, 9};
+        int[ ] array3 = {3, 5};
+        int j = 0;
+        for(int i: tree) {
+            array[j] = i;
+            j++;
+        }
+        assertFalse(tree.isEmpty());
+        assertArrayEquals(array, array2);
+        tree.remove(9);
+        assertFalse(tree.contains(9));
+        j = 0;
+        array = new int[tree.size()];
+        for(int i: tree) {
+            array[j] = i;
+            j++;
+        }
+        assertArrayEquals(array, array3);
+    }
+
+    @Test
+    public  void  testToArray(){
+        Avl<Double> tree = new Avl<Double>();
+        tree.add(5.55);
+        tree.add(9.231);
+        tree.add(3.15);
+        Double[] array;
+        array =  tree.toArray(new Double[tree.size()]);
+        assertEquals((double) array[0], 3.15, 0);
+        assertEquals((double) array[1], 5.55, 0);
+        assertEquals((double) array[2], 9.231, 0);
+    }
+
+    @Test
+    public  void  testToArray2(){
+        Avl<Integer> tree = new Avl<Integer>();
+        tree.add(5);
+        tree.add(9);
+        tree.add(3);
+        Integer[] array;
+        array =  tree.toArray(new Integer[tree.size()-2]);
+        assertEquals((long) array[0], 3);
+        assertEquals((long) array[1], 5);
+        assertEquals((long) array[2], 9);
     }
 }
